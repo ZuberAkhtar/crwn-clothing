c37744c41f0a6e8f1393f09a44ce432127ca02a8
ZuberAkhtar <zuberakhtar@gmail.com>
2021-12-22 23:09:56 +0530

    Adding stripe api with our project

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51K9VPlSGyCvENQhKon3E50yyGDKKA8hP1IGDNwcRhQO0fvKb9ztutVpU13ThI3zy9eQMghvaDwCVLUIQuDWfjimI00TQjozisT';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
